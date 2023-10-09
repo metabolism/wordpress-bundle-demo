26c3fdf96b7ca9f5f3c6ec0ae4430344bdc8cbfa
Jerome Barbato <jerome.barbato@gmail.com>
2023-10-09 12:47:35 +0200

    bugfix

--- webpack.config.js
+++ webpack.config.js
@@ -34,7 +34,7 @@ Encore
 
     // will require an extra script tag for runtime.js
     // but, you probably want this, unless you're building a single-page app
-    .enableSingleRuntimeChunk()
+    .disableSingleRuntimeChunk()
 
     /*
      * FEATURE CONFIG
